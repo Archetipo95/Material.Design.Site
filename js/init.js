16230fd18c90c38845a1325ab5e06c53037aa477
Martin <martin.masevski@gmail.com>
2016-11-07 13:28:15 +0100

    Fix

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav({
-        menuWidth: 240, // Default is 240
+        menuWidth: 300, // Default is 240
         edge: 'left', // Choose the horizontal origin
         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens
